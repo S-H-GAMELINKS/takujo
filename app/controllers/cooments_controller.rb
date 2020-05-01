0ec85ef6b1c9a24b06c85d878e80b019f9799865
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-05-01 18:28:40 +0900

    Create CommentsController

---
+++ app/controllers/cooments_controller.rb
@@ -0,0 +1,21 @@
+class CoomentsController < ApplicationController
+    before_action :set_room
+
+    def create
+        if @room.comments.create comment_params
+            redirect_to @room
+        else
+            redirect_to @room, flash: { error: @comment.errors.full_messages }
+        end
+    end
+
+    private
+
+        def set_room
+            @room = Room.find(params[:id])
+        end
+
+        def comment_params
+            params.require(:comment).permit(:content, :room_id)
+        end
+end
